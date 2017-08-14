95339d0dbd393369fa09377a078304a8f24e80ad
zhanfeng <18272734568@163.com>
2017-08-14 14:42:23 +0800

    Initial Commit

---
+++ HidePoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HidePoint
+//
+//  Created by zhanfeng on 2017/8/14.
+//  Copyright © 2017年 lzf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HidePoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HidePoint
+//
+//  Created by zhanfeng on 2017/8/14.
+//  Copyright © 2017年 lzf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
